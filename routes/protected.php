d216f588274307876da023262f3862a29888934f
Bradley Martin <bradley.r.martin@me.com>
2025-09-30 20:27:15 +1000

    add role routes.

--- routes/protected.php
+++ routes/protected.php
@@ -1,3 +1,13 @@
 <?php
 
 declare(strict_types=1);
+
+use App\Http\Controllers\Role\RoleCreateController;
+use App\Http\Controllers\Role\RoleListController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('roles')->name('roles.')->group(function () {
+    Route::get('/', [RoleListController::class, 'view'])->name('index');
+    Route::get('/create', [RoleCreateController::class, 'view'])->name('create');
+    Route::post('/create', [RoleCreateController::class, 'process'])->name('store');
+});
