f52fb781582e66fa761641442ab38c9810a3eb4a
Bradley Martin <bradley.r.martin@me.com>
2025-09-30 20:23:12 +1000

    add protected routes.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,11 +5,16 @@ declare(strict_types=1);
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
+        then: function (): void {
+            Route::middleware(['web', 'auth'])
+                ->group(base_path('routes/protected.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
