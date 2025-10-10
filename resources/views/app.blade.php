4deb05c8df477da861a9e6032673d7bfc5a0e71c
Bradley Martin <bradley.r.martin@me.com>
2025-10-10 22:12:50 +1100

    U

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,7 +22,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.tsx'])
     @inertiaHead
 </head>
-<body class="font-sans antialiased  min-h-screen">
+<body class="font-sans antialiased  min-h-screen overflow-hidden fixed inset-0">
     @inertia
 </body>
 </html>
