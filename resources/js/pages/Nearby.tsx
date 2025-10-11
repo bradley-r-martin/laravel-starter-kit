86945ea8e0646f1024b5321f556f4cb57b6d8127
Bradley Martin <bradley.r.martin@me.com>
2025-10-11 21:53:00 +1100

    update

--- resources/js/pages/Nearby.tsx
+++ resources/js/pages/Nearby.tsx
@@ -19,6 +19,7 @@ const Nearby: FunctionComponent<NearbyProps> = () => {
     <Drawer radius="xl" opened={opened} onClose={toggle} withCloseButton={false} position='bottom' padding={0} styles={{
        
         content:{
+            backgroundColor: 'black',
             height: 'auto',
                         borderBottomLeftRadius: 0,
                         borderBottomRightRadius: 0,
@@ -26,14 +27,37 @@ const Nearby: FunctionComponent<NearbyProps> = () => {
         }
     }}>
         {/* Drawer content */}
-        {!data && <Scanner onScan={(data) => {
+        {!data && <div className='relative'>
+
+            <div className='absolute inset-14 z-20'>
+            <div className="size-3 rounded-tl border-t-2 border-l-2 border-white absolute top-0 left-0" />
+  <div className="size-3 rounded-tl border-t-2 border-l-2 border-white absolute top-0 right-0 rotate-90" />
+  <div className="size-3 rounded-tl border-t-2 border-l-2 border-white absolute right-0 bottom-0 rotate-180" />
+  <div className="size-3 rounded-tl border-t-2 border-l-2 border-white absolute bottom-0 left-0 -rotate-90" />
+            
+            </div>
+
+    
+            <div
+                            className="animate-scan z-10 absolute top-1/2 -translate-y-1/2 h-1/2 w-full border-t border-white bg-white/20"
+                            style={{
+                                maskImage: 'radial-gradient(52.19% 100% at 50% 0%, rgb(0, 0, 0) 0%, rgba(0, 0, 0, 0) 95.31%)',
+                            }}
+                        />
+           <Scanner 
+        
+        components={{
+            torch:false,
+            finder:false,
+        }}
+        onScan={(data) => {
             setData(data);
 
-        }} />}
+        }} /></div>}
         {data && <div className='p-4'>{data.map((item) => (
             <div key={item.rawValue}>{item.rawValue}</div>
         ))}</div>}
-            <div className='p-4 bg-black/70 absolute bottom-0 left-0 right-0 flex items-center justify-center'>
+            <div className='p-4 bg-black flex items-center justify-center'>
                 <Button onClick={toggle} variant='outline' color="white">Cancel</Button>
             </div>
       </Drawer>
