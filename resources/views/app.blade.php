86d5cffc555a47ebc71e92033811712519e78581
Bradley Martin <bradley.r.martin@me.com>
2025-10-15 18:02:42 +1100

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,7 +30,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.tsx'])
     @inertiaHead
 </head>
-<body class="font-sans antialiased  min-h-dvh overflow-hidden">
+<body class="font-sans antialiased  min-h-dvh  overflow-hidden">
     @inertia
 </body>
 </html>
